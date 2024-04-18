ec9cdbeafa655727ae3b40ba7edc733a42b0cdb6
Smilling Battaglini <vanessabattaglini@gmail.com>
2024-04-18 16:07:30 -0400

    Crear los datos del usuario del Json

--- queries/roommates.js
+++ queries/roommates.js
@@ -4,15 +4,28 @@ import { v4 as uuidv4 } from "uuid";
 
 const urlApi = "https://randomuser.me/api";
 
+//Construir datos del usuario
 const addRoommateQuery = async (req, res) => {
     try {
-        const data = await axios(urlApi);
+        const data = await axios.get(urlApi);
         const userData = data.data.results[0]
-        console.log(user)
+        const id = uuidv4().slice(0, 5);
+        const usuario = {
+            id,
+            nombre: `${userData.name.first} ${userData.name.last}`,
+            email: userData.email,
+            debe: 0,
+            recibe: 0
+        };
+        const roommateJson = JSON.parse(fs.readFileSync('data/roommates.json', 'utf8'));
+        roommateJson.roommates.push(usuario)
+        fs.writeFileSync('data/roommates.json', JSON.stringify(roommateJson));
     } catch (error) {
         console.log(error.message)
     }
 };
+
+
 
 
 
